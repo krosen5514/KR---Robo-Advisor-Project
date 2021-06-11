5e864965792bec7f4afb6b8abac4ceb6329a0534
krosen5514 <84346108+krosen5514@users.noreply.github.com>
2021-06-10 20:46:25 -0400

    final commit for 6.10

--- robo_advisor.py
+++ robo_advisor.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 import requests
+import json
+
 
 load_dotenv()
 ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
@@ -8,5 +10,8 @@ ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 symbol = "MSFT" # ask for a user input
 request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
 response = requests.get(request_url)
-print(type(response))
-print(response.text)
+
+Time_series_data = json.loads(response.text)
+print(Time_series_data)
+
+
