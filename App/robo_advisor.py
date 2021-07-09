53ef620131867c027533cfef0ca6553db81f69a1
krosen5514 <84346108+krosen5514@users.noreply.github.com>
2021-07-08 22:10:58 -0400

    Included data

--- App/robo_advisor.py
+++ App/robo_advisor.py
@@ -1,4 +1,6 @@
 import os
+
+from requests.models import DEFAULT_REDIRECT_LIMIT
 from dotenv import load_dotenv
 import requests
 import json
@@ -10,13 +12,24 @@ ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 symbol = input("Which symbol would you like data for:") 
 request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
 response = requests.get(request_url)
-print(response)
-if response == Response [200]:
-    print("nice")
-else:
-    print("Oops, please select a valid security")
+time_series_data = json.loads(response.text)
 
-Time_series_data = json.loads(response.text)
+print("-------------------------")
+print("SELECTED SYMBOL:", "Meta ")
+print("-------------------------")
+print("REQUESTING STOCK MARKET DATA...")
+date = time_series_data["Meta Data"]["3. Last Refreshed"]
+print("REQUEST AT:", date)
+print("-------------------------")
+print("LATEST CLOSE:",  time_series_data["Time Series (Daily)"][date]["4. close"])
+print("RECENT HIGH: 101,000.00")
+# print("RECENT LOW: $99,000.00")
+# print("-------------------------")
+# print("RECOMMENDATION: BUY!")
+# print("RECOMMENDATION REASON: TODO")
+# print("-------------------------")
+# print("HAPPY INVESTING!")
+# print("-------------------------")
 #print(Time_series_data)
 
 
